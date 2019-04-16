13a031a9a7f0c017af66ab8d7fdd218aea6eeb7e
Angela <angela-denninger@web.de>
2019-04-16 14:52:05 +0200

    transformation

--- src/visualize/showMesh.cpp
+++ src/visualize/showMesh.cpp
@@ -6,21 +6,21 @@ void ShowMesh::init(ml::ApplicationData &app)
 {
 	_mesh_renderer = std::make_unique<MeshRenderer>(app);
 
-	//_mesh_reader = std::make_unique<MeshReader>("../input_data/HaoLi/hand/hand-inputScans/hand-inputScans/");
+	_mesh_reader = std::make_unique<MeshReader>("../input_data/HaoLi/hand/hand-inputScans/hand-inputScans/");// , scale, transform);
 
 	//MeshDataf meshData = MeshIOf::loadFromFile("scans/gates381.ply");
 	MeshDataf meshData = MeshIOf::loadFromFile("../input_data/HaoLi/hand/hand-inputScans/hand-inputScans/meshOfFrame000000.obj");
 	ml::TriMeshf triMesh(meshData);
 
-	triMesh.scale(0.01);
-	ml::mat4f rotation = ml::mat4f::rotationX(90.);
-	triMesh.transform(rotation);
-
 	auto bounding_box = triMesh.computeBoundingBox();
 	ml::mat4f center = ml::mat4f::translation(-bounding_box.getCenter());
+	triMesh.transform(center);
+
+	ml::mat4f scale = ml::mat4f::scale(0.01);
+	ml::mat4f rotation = ml::mat4f::rotationX(90.);
 	ml::mat4f transform = ml::mat4f::translation({ -0.5f, 1.5f, 1.5f });
-	ml::mat4f transformation = transform * center;
-	triMesh.transform(transformation);
+	
+	triMesh.transform(transform * rotation * scale);
 
 	_mesh_renderer->insertMesh("mesh", triMesh);
 }
