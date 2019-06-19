c43c49e563197e58514b48a25bacf39d5dbc7fad
Angela <angela-denninger@web.de>
2019-06-19 13:22:00 +0200

    use isotropic mesh refinement to create a better initial deformation mesh

--- src/algo/mesh_simplification/mesh_simplification.cpp
+++ src/algo/mesh_simplification/mesh_simplification.cpp
@@ -12,26 +12,53 @@
 #include <CGAL/Polyhedron_items_with_id_3.h>
 
 
+#include <CGAL/Polygon_mesh_processing/refine.h>
+#include <CGAL/Polygon_mesh_processing/fair.h>
+#include <CGAL/Polygon_mesh_processing/remesh.h>
+#include <CGAL/Polygon_mesh_processing/border.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polyhedron_3.h>
+
 Mesh createReducedMesh(const Mesh & mesh, int number_of_vertices)
 {
 	auto surface_mesh = convertToCGALMesh(mesh);
-
-	//CGAL::Surface_mesh_simplification::Count_stop_predicate_vertices<SurfaceMesh> stop(number_of_vertices);
-
-	// This is a stop predicate (defines when the algorithm terminates).
-	// In this example, the simplification stops when the number of undirected edges
-	// left in the surface mesh drops below the specified number (1000)
-	CGAL::Surface_mesh_simplification::Count_stop_predicate<SurfaceMesh> stop(number_of_vertices);
-
-	int r = CGAL::Surface_mesh_simplification::edge_collapse(surface_mesh, stop);
+	surface_mesh = createReducedMesh(surface_mesh, number_of_vertices);
 	return convertToTriMesh(surface_mesh);
 }
+
+
+struct halfedge2edge
+{
+	halfedge2edge(const SurfaceMesh& m, std::vector<edge_descriptor>& edges)
+		: m_mesh(m), m_edges(edges)
+	{}
+	void operator()(const halfedge_descriptor& h) const
+	{
+		m_edges.push_back(edge(h, m_mesh));
+	}
+	const SurfaceMesh& m_mesh;
+	std::vector<edge_descriptor>& m_edges;
+};
 
 
 SurfaceMesh createReducedMesh(const SurfaceMesh & mesh, int number_of_vertices)
 {
 	SurfaceMesh surface_mesh = mesh;
-	CGAL::Surface_mesh_simplification::Count_stop_predicate<SurfaceMesh> stop(number_of_vertices);
-	int r = CGAL::Surface_mesh_simplification::edge_collapse(surface_mesh, stop);
+
+	//CGAL::Surface_mesh_simplification::Count_stop_predicate<SurfaceMesh> stop(number_of_vertices);
+	//int r = CGAL::Surface_mesh_simplification::edge_collapse(surface_mesh, stop);
+
+
+	double target_edge_length = 0.05;
+
+	std::vector<edge_descriptor> border;
+	CGAL::Polygon_mesh_processing::border_halfedges(faces(surface_mesh), surface_mesh, boost::make_function_output_iterator(halfedge2edge(surface_mesh, border)));
+	CGAL::Polygon_mesh_processing::split_long_edges(border, target_edge_length, surface_mesh);
+
+	//CGAL::Polygon_mesh_processing::split_long_edges();
+	CGAL::Polygon_mesh_processing::isotropic_remeshing(faces(surface_mesh),
+													   target_edge_length,
+													   surface_mesh,
+													   CGAL::Polygon_mesh_processing::parameters::number_of_iterations(10));// .protect_constraints(true));
 	return surface_mesh;
 }
