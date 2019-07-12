ab2866770654ac8828045d75a9ab1db69a499312
Angela <angela-denninger@web.de>
2019-07-12 15:08:34 +0200

    simplify mesh using make_mesh_3

--- src/algo/mesh_simplification/mesh_simplification.cpp
+++ src/algo/mesh_simplification/mesh_simplification.cpp
@@ -2,30 +2,23 @@
 
 #include "mesh_simplification.h"
 #include "algo/surface_mesh/mesh_convertion.h"
-//
-#include <CGAL/Surface_mesh_simplification/edge_collapse.h>
-#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Count_ratio_stop_predicate.h>
 
-//// Stop-condition policy
-#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Count_stop_predicate.h>
 #include <CGAL/Polyhedron_3.h>
-#include <CGAL/Polyhedron_items_with_id_3.h>
+//#include <CGAL/Polyhedron_items_with_id_3.h>
+#include <CGAL/Polyhedral_mesh_domain_with_features_3.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
-
-#include <CGAL/Polygon_mesh_processing/refine.h>
-#include <CGAL/Polygon_mesh_processing/fair.h>
 #include <CGAL/Polygon_mesh_processing/remesh.h>
 #include <CGAL/Polygon_mesh_processing/border.h>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Polyhedron_3.h>
 
+#include <CGAL/Mesh_criteria_3.h>
+#include <CGAL/Mesh_triangulation_3.h>
+#include <CGAL/Mesh_complex_3_in_triangulation_3.h>
 
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/boost/graph/graph_traits_PolyMesh_ArrayKernelT.h>
 #include <CGAL/poisson_surface_reconstruction.h>
-#include <CGAL/IO/read_xyz_points.h>
-//#include <CGAL/Mesh_criteria_3.h>
-//#include <CGAL/make_mesh_3.h>
+#include <CGAL/make_mesh_3.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel ExactInexactKernel;
 
 Mesh createReducedMesh(const Mesh & mesh, double target_edge_length)
 {
@@ -51,16 +44,16 @@ struct halfedge2edge
 
 
 
-
+/*
 SurfaceMesh createReducedMeshPoisonSurfaceReconstruction(const SurfaceMesh & mesh, double target_edge_length)
 {
 	assert(surface_mesh.property_map<vertex_descriptor, Vector>("v:normal").second);
 	auto normals = mesh.property_map<vertex_descriptor, Vector>("v:normal").first;
 
-	typedef CGAL::Exact_predicates_inexact_constructions_kernel RemeshKernel;
+	typedef CGAL::Exact_predicates_inexact_constructions_kernel PoisonRemeshKernel;
 	//typedef CGAL::Simple_cartesian<double> RemeshKernel;
-	typedef RemeshKernel::Point_3 RemeshPoint;
-	typedef RemeshKernel::Vector_3 RemeshVector;
+	typedef PoisonRemeshKernel::Point_3 RemeshPoint;
+	typedef PoisonRemeshKernel::Vector_3 RemeshVector;
 	typedef std::pair<RemeshPoint, RemeshVector> PointWithNormal;
 	std::vector<PointWithNormal> points;
 	for (auto & v : mesh.vertices())
@@ -70,7 +63,7 @@ SurfaceMesh createReducedMeshPoisonSurfaceReconstruction(const SurfaceMesh & mes
 		points.emplace_back(std::make_pair(RemeshPoint(p.x(), p.y(), p.z()), RemeshVector(n.x(), n.y(), n.z())));
 	}
 
-	CGAL::Polyhedron_3<RemeshKernel> remeshed_surface;
+	CGAL::Polyhedron_3<PoisonRemeshKernel> remeshed_surface;
 	double average_spacing = CGAL::compute_average_spacing<CGAL::Sequential_tag>
 		(points, 6, CGAL::parameters::point_map(CGAL::First_of_pair_property_map<PointWithNormal>()));
 
@@ -78,20 +71,17 @@ SurfaceMesh createReducedMeshPoisonSurfaceReconstruction(const SurfaceMesh & mes
 	double 	sm_radius = 30;
 	double 	sm_distance = 0.375;
 	bool success = CGAL::poisson_surface_reconstruction_delaunay(points.begin(), points.end(),
-																	CGAL::First_of_pair_property_map<PointWithNormal>(),
-																	CGAL::Second_of_pair_property_map<PointWithNormal>(),
-																	remeshed_surface,
+																 CGAL::First_of_pair_property_map<PointWithNormal>(),
+																 CGAL::Second_of_pair_property_map<PointWithNormal>(),
+																 remeshed_surface,
 																	average_spacing,
 																	sm_angle,
 																	sm_radius,
 																	sm_distance);
 
-	//bool success = CGAL::poisson_surface_reconstruction_delaunay(surface_mesh.vertices().begin(), surface_mesh.vertices().end(),
-	//															 surface_mesh.points().begin(),
-	//															 normals.begin(),
-	//															 remeshed_surface, target_edge_length);
 
 	std::cout << "success " << success << std::endl;
+
 	SurfaceMesh surface_mesh;
 	CGAL::copy_face_graph(remeshed_surface, surface_mesh);
 	auto add_normals = surface_mesh.add_property_map<vertex_descriptor, Vector>("v:normal", Vector(0., 0., 0.)).first;
@@ -101,23 +91,49 @@ SurfaceMesh createReducedMeshPoisonSurfaceReconstruction(const SurfaceMesh & mes
 
 	std::cout << "count of surface mesh vertices " << surface_mesh.number_of_vertices() << std::endl;
 	return surface_mesh;
-}
+}*/
 
 
 SurfaceMesh createReducedMeshRemeshing(const SurfaceMesh & mesh, double target_edge_length)
-{	
+{
+	// Polyhedron type
+	typedef CGAL::Mesh_polyhedron_3<ExactInexactKernel>::type Polyhedron;
+	typedef CGAL::Polyhedral_mesh_domain_with_features_3<ExactInexactKernel> MeshDomain;
+	// Triangulation
+	typedef CGAL::Mesh_triangulation_3<MeshDomain>::type Tr;
+	typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr, MeshDomain::Corner_index, MeshDomain::Curve_index> C3t3;
+	// Criteria
+	typedef CGAL::Mesh_criteria_3<Tr> MeshCriteria;
 
-	// Mesh criteria
+	Polyhedron poly;
+	CGAL::copy_face_graph(mesh, poly);
+	// Create a vector with only one element: the pointer to the polyhedron.
+	std::vector<Polyhedron*> poly_ptrs_vector(1, &poly);
+	MeshDomain domain(poly_ptrs_vector.begin(), poly_ptrs_vector.end());
 
-	//CGAL::Mesh_criteria_3<SurfaceMesh> criteria(0.025, //edge_size
-	//											25,//facet_angle = 
-	//											0.1,//facet_size
-	//											0.001);//facet_distance
+	MeshCriteria criteria(CGAL::parameters::edge_size = 0.1,
+						  CGAL::parameters::facet_angle = 20,
+						  CGAL::parameters::facet_size = 0.7,
+						  CGAL::parameters::facet_distance = 0.01,
+						  CGAL::parameters::cell_radius_edge_ratio = 2,
+						  CGAL::parameters::cell_size = 1.5);
+
+	// Mesh generation
+	C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria, CGAL::parameters::no_perturb(), CGAL::parameters::no_exude());	
+
+	SurfaceMesh surface_mesh;
+	CGAL::facets_in_complex_3_to_triangle_mesh(c3t3, surface_mesh);
+
+	auto add_normals = surface_mesh.add_property_map<vertex_descriptor, Vector>("v:normal", Vector(0., 0., 0.)).first;
+	CGAL::Polygon_mesh_processing::compute_vertex_normals(surface_mesh, add_normals);
+	surface_mesh.add_property_map<vertex_descriptor, ml::vec4f>("v:color", ml::vec4f(1., 1., 1., 1.)).first;
+	surface_mesh.add_property_map<edge_descriptor, ml::vec4f>("e:color", ml::vec4f(1., 1., 1., 1.)).first;
+
+	std::cout << "count of surface mesh vertices " << surface_mesh.number_of_vertices() << std::endl;
+	return surface_mesh;
 
 	//// Mesh generation
 	//auto refined_mesh = CGAL::make_mesh_3<SurfaceMesh>(surface_mesh, criteria, CGAL::parameters::no_perturb(), CGAL::parameters::no_exude());
-
-	return mesh;
 }
 
 
@@ -148,8 +164,8 @@ SurfaceMesh createReducedMesh(const SurfaceMesh & mesh, double target_edge_lengt
 	SurfaceMesh surface_mesh = mesh;
 	try {
 		//return createReducedMeshIsotropicRemeshing(mesh, target_edge_length);
-		return createReducedMeshPoisonSurfaceReconstruction(mesh, target_edge_length);
-		//return createReducedMeshRemeshing(mesh, target_edge_length);
+		//return createReducedMeshPoisonSurfaceReconstruction(mesh, target_edge_length);
+		return createReducedMeshRemeshing(mesh, target_edge_length);
 	}
 	catch (...) {
 		return mesh;
