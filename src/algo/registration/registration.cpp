8f953be0cba5ae1a7068c5526db6049133d6ba95
Angela <angela-denninger@web.de>
2019-07-18 10:25:33 +0200

    log current config

--- src/algo/registration/registration.cpp
+++ src/algo/registration/registration.cpp
@@ -6,6 +6,23 @@
 #include "algo/registration/embedded_deformation/ed.h"
 #include "algo/registration/arap/arap.h"
 
+void logRegistrationOptions(std::shared_ptr<FileWriter> logger, const RegistrationOptions & registration_options)
+{
+	std::stringstream ss;
+	ss << "Options: " << std::endl
+		<< " coef " << " smooth: " << registration_options.smooth
+		<< " fit: " << registration_options.fit
+		<< " conf: " << registration_options.conf << std::endl
+		<< " deformation graph " << "edge length: " << registration_options.dg_options.edge_length
+		<< " number of interpolated neighbors (k): " << registration_options.dg_options.number_of_interpolation_neighbors << std::endl
+		<< " max iterations: " << registration_options.max_iterations << std::endl
+		<< " ignore border vertices: " << registration_options.ignore_deformation_graph_border_vertices << std::endl
+		<< " find correspondence " << " max distance: " << registration_options.correspondence_max_distance
+		<< " max angle: " << registration_options.correspondence_max_angle_deviation << std::endl
+		<< " evaluate residuals: " << registration_options.evaluate_residuals << std::endl;
+	logger->write(ss.str());
+}
+
 std::unique_ptr<IRegistration> createRegistration(const SurfaceMesh & source, 
 												  const SurfaceMesh & target, 												  
 												  RegistrationType registration_type,
@@ -14,6 +31,7 @@ std::unique_ptr<IRegistration> createRegistration(const SurfaceMesh & source,
 												  std::shared_ptr<FileWriter> logger,
 												  std::vector<vertex_descriptor> fixed_positions)
 {
+	logRegistrationOptions(logger, registration_options);
 	if (registration_type == RegistrationType::ED)
 		return std::make_unique<ED::EmbeddedDeformation>(source, target, options, registration_options.dg_options.edge_length, registration_options.evaluate_residuals, logger);
 	else if (registration_type == RegistrationType::ED_WithoutICP)
@@ -35,6 +53,7 @@ std::unique_ptr<IRegistration> createRegistration(const SurfaceMesh & source,
 												  RegistrationOptions registration_options,
 												  std::shared_ptr<FileWriter> logger)
 {
+	logRegistrationOptions(logger, registration_options);
 	if (registration_type == RegistrationType::ED)
 		return std::make_unique<ED::EmbeddedDeformation>(source, target, deformation_graph, options, registration_options.evaluate_residuals, logger);
 	else if (registration_type == RegistrationType::ARAP)
