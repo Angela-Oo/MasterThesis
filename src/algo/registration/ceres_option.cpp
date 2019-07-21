896d3feb81a1ac2dcd1be09ecfb3b18903f99a7f
Angela <angela-denninger@web.de>
2019-07-21 23:30:20 +0200

    found black magic option to enable multithreading set ceres option num threads to number of cores

--- src/algo/registration/ceres_option.cpp
+++ src/algo/registration/ceres_option.cpp
@@ -2,15 +2,16 @@
 
 ceres::Solver::Options ceresOption() {
 	ceres::Solver::Options options;
-	options.sparse_linear_algebra_library_type = ceres::EIGEN_SPARSE;
+	options.sparse_linear_algebra_library_type = ceres::SparseLinearAlgebraLibraryType::SUITE_SPARSE;// ceres::EIGEN_SPARSE;
 	options.minimizer_type = ceres::MinimizerType::TRUST_REGION;
 	options.trust_region_strategy_type = ceres::TrustRegionStrategyType::LEVENBERG_MARQUARDT;
 	options.line_search_direction_type = ceres::LineSearchDirectionType::LBFGS;
-	options.linear_solver_type = ceres::LinearSolverType::DENSE_SCHUR; //ceres::LinearSolverType::CGNR; // SPARSE_SCHUR // sparse normal cholesky is not threaded
-	options.preconditioner_type = ceres::PreconditionerType::JACOBI;// SCHUR_JACOBI;
+	options.linear_solver_type = ceres::LinearSolverType::SPARSE_SCHUR; //DENSE_SCHUR; //ceres::LinearSolverType::CGNR; // SPARSE_SCHUR // sparse normal cholesky is not threaded
+	options.preconditioner_type = ceres::PreconditionerType::JACOBI;// ceres::PreconditionerType::JACOBI;// SCHUR_JACOBI;
 	options.max_num_iterations = 50;// 100;
 	options.logging_type = ceres::LoggingType::SILENT;
 	options.minimizer_progress_to_stdout = false;
 	options.function_tolerance = 1e-6;// 1e-6;
+	options.num_threads = 12;
 	return options;
 }
