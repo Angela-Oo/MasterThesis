9f1ef6543a7ab84ddda9a2c2d4556e0becf3b10b
Angela <angela.denninger@googlemail.com>
2019-08-02 23:23:17 +0200

    tests

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -59,61 +59,21 @@ TEST_F(RefineOneEdge, TestEdgesToRefine)
 	EXPECT_EQ(edges[0], e);
 }
 
-TEST_F(RefineOneEdge, TestRefine)
-{
-	auto smooth_cost_property_map = mesh.add_property_map<edge_descriptor, double>("e:smooth_cost", 0.);
-	auto he = mesh.halfedge(fids[0]);
-
-	auto e = mesh.edge(he);
-	smooth_cost_property_map.first[e] = 1.;
-
-	auto refined = Registration::refineDeformationGraph(mesh);
-
-	EXPECT_EQ(refined.number_of_faces(), 4);
-	EXPECT_EQ(refined.number_of_vertices(), 5);
-}
-
-
-
-
-
-
-// Mesh structure
-//         v0
-//       /    \
-//     v2 ---- v1
+//TEST_F(RefineOneEdge, TestRefine)
+//{
+//	auto smooth_cost_property_map = mesh.add_property_map<edge_descriptor, double>("e:smooth_cost", 0.);
+//	auto he = mesh.halfedge(fids[0]);
 //
-class RefineFace : public ::testing::Test {
-public:
-	SurfaceMesh mesh;
-	std::vector<vertex_descriptor> vids;
-	std::vector<face_descriptor> fids;
-public:
-	RefineFace() {
-		// initialization code here
+//	auto e = mesh.edge(he);
+//	smooth_cost_property_map.first[e] = 1.;
+//
+//	auto refined = Registration::refineDeformationGraph(mesh);
+//
+//	EXPECT_EQ(refined.number_of_faces(), 4);
+//	EXPECT_EQ(refined.number_of_vertices(), 5);
+//}
 
-		vids.push_back(mesh.add_vertex(Point( 0., 3., 0.)));
-		vids.push_back(mesh.add_vertex(Point(-2., 0., 0.)));
-		vids.push_back(mesh.add_vertex(Point( 2., 0., 0.)));
 
-		fids.push_back(mesh.add_face(vids[0], vids[1], vids[2]));
-	}
-
-	void SetUp() {
-		// code here will execute just before the test ensues 
-	}
-
-	void TearDown() {
-		// code here will be called just after the test completes
-		// ok to through exceptions from here if need be
-	}
-
-	~RefineFace() {
-		// cleanup any pending stuff, but no exceptions allowed
-	}
-
-	// put in any custom data members that you need 
-};
 
 
 
@@ -168,17 +128,39 @@ public:
 
 
 
-//TEST_F(RefineThreeEdges, TestEdgesToRefine)
-//{
-//	auto smooth_cost_property_map = mesh.add_property_map<edge_descriptor, double>("e:smooth_cost", 0.);
-//	auto he = mesh.halfedge(fids[0]);
-//	auto e = mesh.edge(he);
-//	smooth_cost_property_map.first[e] = 1.;
-//
-//	auto edges = Registration::getEdgesToRefine(mesh);
-//	EXPECT_EQ(edges.size(), 1);
-//	EXPECT_EQ(edges[0], e);
-//}
+TEST_F(RefineThreeEdges, TestTwoEdgesToRefine)
+{
+	auto smooth_cost_property_map = mesh.add_property_map<edge_descriptor, double>("e:smooth_cost", 0.);
+
+	auto he = mesh.halfedge(fids[1]);
+	auto e0 = mesh.edge(he);
+	auto e1 = mesh.edge(mesh.next(he));
+	smooth_cost_property_map.first[e0] = 1.;
+	smooth_cost_property_map.first[e1] = 1.;
+
+	auto edges = Registration::getEdgesToRefine(mesh);
+	EXPECT_EQ(edges.size(), 2);
+	EXPECT_EQ(edges[0], e1);
+	EXPECT_EQ(edges[1], e0);
+}
+
+TEST_F(RefineThreeEdges, TestTwoEdgesToSplit)
+{
+	auto smooth_cost_property_map = mesh.add_property_map<edge_descriptor, double>("e:smooth_cost", 0.);
+
+	auto he = mesh.halfedge(fids[1]);
+	auto e0 = mesh.edge(he);
+	auto e1 = mesh.edge(mesh.next(he));
+	auto e2 = mesh.edge(mesh.prev(he));
+	smooth_cost_property_map.first[e0] = 1.;
+	smooth_cost_property_map.first[e1] = 1.;
+
+	auto edges = Registration::getEdgesToSplit(mesh);
+	EXPECT_EQ(edges.size(), 3);
+	EXPECT_EQ(edges[0], e1);
+	EXPECT_EQ(edges[1], e0);
+	EXPECT_EQ(edges[2], e2);
+}
 //
 //TEST_F(RefineThreeEdges, TestRefine)
 //{
