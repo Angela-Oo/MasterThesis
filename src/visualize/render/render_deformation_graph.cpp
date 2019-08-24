f89a38697b0b313f4928649d1c6c9cd68dc1b99d
Angela <angela.denninger@googlemail.com>
2019-08-24 21:45:32 +0200

    color rigidity weight

--- src/visualize/render/render_deformation_graph.cpp
+++ src/visualize/render/render_deformation_graph.cpp
@@ -5,7 +5,7 @@ using namespace Registration;
 namespace Visualize {
 
 
-std::tuple<double, double> getMeanAndMaxVertexCost(const SurfaceMesh & mesh, std::string cost_name)
+std::tuple<bool, double, double> getMeanAndMaxVertexCost(const SurfaceMesh & mesh, std::string cost_name)
 {
 	if (mesh.number_of_vertices() > 0 && mesh.property_map<vertex_descriptor, double>(cost_name).second) {
 		auto costs = mesh.property_map<vertex_descriptor, double>(cost_name).first;
@@ -17,63 +17,47 @@ std::tuple<double, double> getMeanAndMaxVertexCost(const SurfaceMesh & mesh, std
 				max_cost = costs[v];
 		}
 		mean_cost /= mesh.number_of_vertices();
-		return std::make_tuple(mean_cost, max_cost);
+		return std::make_tuple(true, mean_cost, max_cost);
 	}
-	return std::make_tuple(0.1, 0.1);
+	return std::make_tuple(false, 0.1, 0.1);
 }
 
-std::tuple<double, double> getMeanAndMaxConfCost(const SurfaceMesh & mesh)
+std::tuple<bool, double, double> getMeanAndMaxEdgeValue(const SurfaceMesh & mesh, std::string property_name)
+{
+	auto values = mesh.property_map<edge_descriptor, double>(property_name);
+
+	if (mesh.number_of_vertices() > 0 && values.second) {
+		double mean = 0.;
+		double max = 0.0;
+		for (auto e : mesh.edges()) {
+			mean += values.first[e];
+			if (values.first[e] > max)
+				max = values.first[e];
+		}
+		mean /= mesh.number_of_edges();
+		return std::make_tuple(true, mean, max);
+	}
+	return std::make_tuple(false, 0.1, 0.1);
+}
+
+std::tuple<bool, double, double> getMeanAndMaxConfCost(const SurfaceMesh & mesh)
 {
 	return getMeanAndMaxVertexCost(mesh, "v:conf_cost");
 }
 
-std::tuple<double, double> getMeanAndMaxFitCost(const SurfaceMesh & mesh)
+std::tuple<bool, double, double> getMeanAndMaxFitCost(const SurfaceMesh & mesh)
 {
 	return getMeanAndMaxVertexCost(mesh, "v:fit_cost");
 }
 
 std::tuple<bool, double, double> getMeanAndMaxSmoothCost(const SurfaceMesh & mesh)
 {
-	auto smooth_costs = mesh.property_map<edge_descriptor, double>("e:smooth_cost");
-	if (mesh.number_of_vertices() > 0 && smooth_costs.second) {	
-		double mean_smooth_cost = 0.;
-		double max_smooth_cost = 0.0;
-		for (auto e : mesh.edges()) {
-			mean_smooth_cost += smooth_costs.first[e];
-			if (smooth_costs.first[e] > max_smooth_cost)
-				max_smooth_cost = smooth_costs.first[e];
-		}
-		mean_smooth_cost /= mesh.number_of_edges();
-		return std::make_tuple(true, mean_smooth_cost, max_smooth_cost);
-	}
-	return std::make_tuple(false, 0.1, 0.1);
+	return getMeanAndMaxEdgeValue(mesh, "e:smooth_cost");
 }
 
-double getReferenceCost(const SurfaceMesh & mesh, bool use_only_smooth_cost)
+std::tuple<bool, double, double> getMeanAndMaxRigidityWeight(const SurfaceMesh & mesh)
 {
-	if (mesh.property_map<edge_descriptor, double>("e:smooth_cost").second) {
-		bool valid;
-		double mean_smooth_cost;
-		double max_smooth_cost;
-		std::tie(valid, mean_smooth_cost, max_smooth_cost) = getMeanAndMaxSmoothCost(mesh);
-
-		if (valid) {
-			if (use_only_smooth_cost) {
-				double reference_cost = mean_smooth_cost * 10.;
-				//std::cout << "max smooth costs: " << max_smooth_cost << " reference smooth cost " << reference_cost << " ";
-				return reference_cost;
-			}
-			else {
-				double mean_fit_cost;
-				double max_fit_cost;
-				std::tie(mean_fit_cost, max_fit_cost) = getMeanAndMaxFitCost(mesh);
-				auto reference_cost = std::max(mean_fit_cost, mean_smooth_cost) * 10.;
-				std::cout << "max costs: fit " << max_fit_cost << " smooth " << max_smooth_cost << " reference cost " << reference_cost << " ";
-				return reference_cost;
-			}
-		}
-	}
-	return 0.;
+	return getMeanAndMaxEdgeValue(mesh, "e:rigidity");
 }
 
 //-----------------------------------------------------------------------------
@@ -105,17 +89,20 @@ void setVertexColorOfUnusedVerticesToBlack(SurfaceMesh & mesh)
 //	}
 //}
 
-void setVertexColorBasedOnFitCost(SurfaceMesh & mesh, double reference_cost)
+void setVertexColorBasedOnFitCost(SurfaceMesh & mesh)
 {
-	auto property_fit = mesh.property_map<vertex_descriptor, double>("v:fit_cost");
-	if (property_fit.second)
-	{
-		auto fit_costs = property_fit.first;
-		auto colors = mesh.property_map<vertex_descriptor, ml::vec4f>("v:color").first;
+	bool valid;
+	double mean_cost;
+	double max_cost;
+	std::tie(valid, mean_cost, max_cost) = getMeanAndMaxVertexCost(mesh, "v:fit_cost");
 
+	auto fit_costs = mesh.property_map<vertex_descriptor, double>("v:fit_cost").first;
+	if (valid)
+	{
+		auto colors = mesh.property_map<vertex_descriptor, ml::vec4f>("v:color").first;
 		for (auto & v : mesh.vertices())
 		{
-			double error = (reference_cost > 0.) ? (fit_costs[v] / reference_cost) : fit_costs[v];
+			double error = (max_cost > 0.) ? (fit_costs[v] / max_cost) : fit_costs[v];
 			error = std::min(1., error);
 			colors[v] = errorToRGB(error);
 		}
@@ -123,34 +110,54 @@ void setVertexColorBasedOnFitCost(SurfaceMesh & mesh, double reference_cost)
 }
 
 
-void setVertexColor(SurfaceMesh & mesh, double reference_cost, bool use_only_smooth_cost)
+void setVertexColor(SurfaceMesh & mesh, bool use_only_smooth_cost)
 {
 	// add color property if not already exists
 	auto vertex_color = errorToRGB(0.);
 	auto colors = mesh.add_property_map<vertex_descriptor, ml::vec4f>("v:color", vertex_color).first;
 	if (!use_only_smooth_cost) {
-		setVertexColorBasedOnFitCost(mesh, reference_cost);
+		setVertexColorBasedOnFitCost(mesh);
 	}
-	//else {		
-	//	for (auto & v : mesh.vertices())
-	//	{
-	//		colors[v] = errorToRGB(0.);
-	//	}
-	//}
-
 	setVertexColorOfUnusedVerticesToBlack(mesh);
-	//setVertexColorWithSmallWeightsToWhite(mesh);
 }
 
 
-void setEdgeColor(SurfaceMesh & mesh, double reference_cost)
+void setEdgeColorToSmoothCost(SurfaceMesh & mesh)
 {
 	auto edge_colors = mesh.property_map<edge_descriptor, ml::vec4f>("e:color");
 	auto smooth_costs = mesh.property_map<edge_descriptor, double>("e:smooth_cost");
-	if (edge_colors.second && smooth_costs.second) {
+	bool valid;
+	double mean_smooth_cost;
+	double max_smooth_cost;
+	std::tie(valid, mean_smooth_cost, max_smooth_cost) = getMeanAndMaxEdgeValue(mesh, "e:smooth_cost");
+
+	if (valid && edge_colors.second && smooth_costs.second) {
+		double reference_cost = mean_smooth_cost * 10.;
 		for (auto & e : mesh.edges())
 		{
 			double error = (reference_cost > 0.) ? (smooth_costs.first[e] / reference_cost) : smooth_costs.first[e];
+			error = std::min(1., error);
+			edge_colors.first[e] = errorToRGB(error);
+		}
+	}
+}
+
+
+void setEdgeColorToRigidityValue(SurfaceMesh & mesh)
+{
+	auto edge_colors = mesh.property_map<edge_descriptor, ml::vec4f>("e:color");
+	auto rigidity = mesh.property_map<edge_descriptor, double>("e:rigidity");
+	bool valid;
+	double mean, max;
+	std::tie(valid, mean, max) = getMeanAndMaxEdgeValue(mesh, "e:rigidity");
+
+	if (valid && edge_colors.second && rigidity.second) {
+		double reference_cost = mean * 10.;
+		for (auto & e : mesh.edges())
+		{
+			double error = std::max(0., (100. - rigidity.first[e]));
+			error = error * 0.01;
+			error = pow(error, 3);
 			error = std::min(1., error);
 			edge_colors.first[e] = errorToRGB(error);
 		}
@@ -161,9 +168,9 @@ void setEdgeColor(SurfaceMesh & mesh, double reference_cost)
 
 void setDeformationGraphColor(SurfaceMesh & mesh, bool use_only_smooth_cost)
 {
-	double reference_cost = getReferenceCost(mesh, use_only_smooth_cost);
-	setVertexColor(mesh, reference_cost, use_only_smooth_cost);
-	setEdgeColor(mesh, reference_cost);
+	setVertexColor(mesh, use_only_smooth_cost);
+	setEdgeColorToRigidityValue(mesh);
+	//setEdgeColorToSmoothCost(mesh);
 }
 
 }
