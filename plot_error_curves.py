0343993751f8522686fe29427d62573d79e8f1a0
Angela <angela.denninger@googlemail.com>
2019-09-12 22:02:08 +0200

    plot error curves

--- plot_error_curves.py
+++ plot_error_curves.py
@@ -13,36 +13,63 @@ def getErrorLines(lines):
     for line in lines:
         if re.search('error: mean', line):
             error_lines.append(line)
-            print(line)
+            #print(line)
         m = re.search('(frame \d+) solved', line)
         if m:
             error_lines.append(m.group(1))
-            print(m.group(1))
+            #print(m.group(1))
+    return error_lines
 
 
-def parseError(log):
-    lines = readLogFileEnd(log, 80)
+def parseError(lines):
     log_dict = dict()
-    m = re.search('error: mean (\d+(\.\d*)?|\.\d+)', lines)
-    log_dict["error mean"] = m.group(1) if m else "unknown"
 
-    m = re.search('variance (\d+(\.\d*)?|\.\d+)', lines)
-    log_dict["error variance"] = m.group(1) if m else "unknown"
+    float_pattern = '[-+]?(?:(?:\d*\.\d+)|(?:\d+\.?))(?:[Ee][+-]?\d+)?'
 
-    m = re.search('median (\d+(\.\d*)?|\.\d+)', lines)
-    log_dict["error median"] = m.group(1) if m else "unknown"
+    #m = re.search('error: mean (\d+(\.\d*)?|\.\d+)', lines)
+    m = re.search('error: mean (' + float_pattern + ')', lines)
+    log_dict["error mean"] = float(m.group(1)) if m else "unknown"
 
-    m = re.search('number of deformation graph nodes (\d+(\.\d*)?|\.\d+)', lines)
-    log_dict["nodes"] = m.group(1) if m else ""
+    m = re.search('variance (' + float_pattern + ')', lines)
+    log_dict["error variance"] = float(m.group(1)) if m else "unknown"
 
-    m = re.search('time: \(h:min:s\) (\d+:\d+:\d+)   \-   finished registration', lines)
-    log_dict["time"] = m.group(1) if m else ""
+    m = re.search('median (' + float_pattern + ')', lines)
+    log_dict["error median"] = float(m.group(1)) if m else "unknown"
+
+    m = re.search('max (' + float_pattern + ')', lines)
+    log_dict["error max"] = float(m.group(1)) if m else "unknown"
     return log_dict
+
+def getErrorForEachFrame(lines):
+    from itertools import groupby
+    split_at_frames = []
+    for match, group in groupby(lines, lambda x : re.search("(frame \d+)", x)):
+        if not match:
+            split_at_frames.append(list(group))
+        else:
+            split_at_frames[-1].append(match.group(1))
+
+    error_dict = dict()
+    for frame in split_at_frames:
+        error_dict[frame[-1]] = parseError(frame[-2])
+
+    from collections import defaultdict
+    test_error_dict = defaultdict(list)
+    for frame in split_at_frames:
+        error = parseError(frame[-2])
+        for key in error:
+            test_error_dict[key].append(error[key])
+        m = re.search('frame (\d+)', frame[-1])
+        if m:
+            test_error_dict['frame'].append(int(m.group(1)))
+
+    return test_error_dict
+    #return error_dict
 
 def parseLogFile(log):
     lines = readLogFile(log)
-    return getErrorLines(lines)
-
+    error_lines = getErrorLines(lines)
+    return getErrorForEachFrame(error_lines)
 
 def getAllLogFiles(path):
     log_files = []
@@ -55,14 +82,38 @@ def getAllLogFiles(path):
 
 
 
-path = "images/results_10_09"
+path = "images/run_2019_09_10"
 log_files = getAllLogFiles(path)
 
 for x in log_files:
     print(x)
 
-dicts = []
+dicts = dict()
 for log in log_files:
+    key = log.replace(path + '\\', '')
+    key = key.replace('\\', '_')
     log_dict = parseLogFile(log)
-    dicts.append(log_dict)
+    dicts[key] = log_dict
+
+
+import matplotlib.pyplot as plt
+import sys
+import os
+
+#data = {'velocity' : [2,4,6,8,12,50],
+#        'time' : [12, 12, 12, 16, 18, 210]}
+
+plt.figure(1)
+
+#data = next(iter(dicts.values()))
+for key in dicts:
+    #if "hand" in key:
+    if True:
+        data = dicts[key]
+        if len(data['frame']) == len(data['error mean']):
+            plt.plot(data['frame'] ,data['error mean'])
+            plt.gcf().autofmt_xdate()
+
+plt.title('frame vs mean error')
+plt.show()
 
